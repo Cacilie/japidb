aed6026b54301652657bfad9f31867ae93c31922
Antonio Arroyo <hi.ed@hotmail.com>
2024-07-06 22:00:18 -0400

    Increase unit testing for Collection class

--- tests/Collection.spec.ts
+++ tests/Collection.spec.ts
@@ -23,6 +23,32 @@ describe("Collection", () => {
         expect(collection1.get(1).value).toBe("1")
     })
 
+    it('saves data and then removes a record', () => {
+        const collection4 = new Collection("collection4", "primary")
+        const record0 = {
+            "primary": 1,
+            "value": "1"
+        }
+
+        collection4.save(record0)
+
+        expect(collection4.get(1).value).toBe("1")
+        collection4.remove(1)
+        expect(collection4.find().length).toBe(0)
+    })
+
+    it('saves data and then retrieves with a find', () => {
+        const collection3 = new Collection("collection3", "primary")
+        const record0 = {
+            "primary": 1,
+            "value": "1"
+        }
+
+        collection3.save(record0)
+
+        expect(collection3.find({"value" : "1"}).length).toBe(1)
+    })
+
     it('record is not saved if it does not include primary key', () => {
         const collection2 = new Collection("collection2", "primary")
         const record0 = {
